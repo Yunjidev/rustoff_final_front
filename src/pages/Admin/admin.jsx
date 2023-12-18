89e0737fbb995715a7904ae4fac1f371b0cc50a5
Yunjidev <florian.vancamp@gmail.com>
2023-12-18 17:56:51 +0100

    Ajout api3

--- src/pages/Admin/admin.jsx
+++ src/pages/Admin/admin.jsx
@@ -6,8 +6,8 @@ import { FaFileCircleXmark } from "react-icons/fa6";
 import { TbFileEuro } from "react-icons/tb";
 import { FaImages } from "react-icons/fa";
 
-import { API_URL } from '../../services/axios-config';
-
+// import { API_URL } from '../../stores/apiUrl';
+import { API_URL } from '../../stores/apiUrl';
 
 const DashboardComponent = () => {
   const [numUsers, setNumUsers] = useState(0);
@@ -21,6 +21,8 @@ const DashboardComponent = () => {
   const [loading, setLoading] = useState(true);
 
   // const API_URL = `${import.meta.env.VITE_BASE_URL}/api/v1`;
+  
+
   const fetchData = async () => {
     try {
       const [userStats, quoteStats, processedCount, unprocessedCount, orderStats, itemStats, recentOrdersData, usersData] = await Promise.all([
