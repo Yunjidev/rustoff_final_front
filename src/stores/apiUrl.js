ceb095fd3c7a03d34c2b7244e5c573c7ee791599
Yunjidev <florian.vancamp@gmail.com>
2023-12-18 17:07:03 +0100

    modifurl

--- src/stores/apiUrl.js
+++ src/stores/apiUrl.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://rustoff-final-app-e6752fce9f21.herokuapp.com/"
+export const API_URL = "https://rustoff-final-app-e6752fce9f21.herokuapp.com"
 // export const API_URL = "https://rustoffback-d3677a58098b.herokuapp.com"
